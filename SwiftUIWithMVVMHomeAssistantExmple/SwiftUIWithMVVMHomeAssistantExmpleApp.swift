3eb845babe6f8bca00150c9ca40e802598160da0
cohenzuri <57574358+cohenzuri@users.noreply.github.com>
2022-12-03 10:29:39 +0200

    Initial Commit

---
+++ SwiftUIWithMVVMHomeAssistantExmple/SwiftUIWithMVVMHomeAssistantExmpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithMVVMHomeAssistantExmpleApp.swift
+//  SwiftUIWithMVVMHomeAssistantExmple
+//
+//  Created by zuri cohen on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithMVVMHomeAssistantExmpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
